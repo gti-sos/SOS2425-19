16a41966bbab9e788cf27cc5a67a4a6896b481fd
marrodcar35 <marrodcar35.alum.us.es>
2025-04-18 17:18:35 +0200

    correccion url MRC

--- src/front/src/routes/+page.svelte
+++ src/front/src/routes/+page.svelte
@@ -2,11 +2,11 @@
     <p><a href="https://sos2425-19.onrender.com/about/">Project About</a></p>
     <p><a href="https://sos2425-19.onrender.com/api/v1/sanctions-and-points-stats/">API-DLC</a></p>
     <p><a href="https://sos2425-19.onrender.com/api/v1/ownerships-changes-stats/">API-JVF</a></p>
-    <p><a href="https://sos2425-19.onrender.com/api/v1/accident-rate-2023-stats/">API-MRC</a></p>
+    <p><a href="https://sos2425-19.onrender.com/api/v1/accident-rate-stats/">API-MRC</a></p>
     
     <p> Daniel Lebrero Carcela,<b>Fuente de datos:</b> sanctions-and-points-stats</p>
     
     <p><a href="https://sos2425-19.onrender.com/api/v1/sanctions-and-points-stats/docs">https://sos2425-19.onrender.com/api/v1/sanctions-and-points-stats/docs</a> (developed by Daniel Lebrero Carcela)</p>
     <p><a href="https://sos2425-19.onrender.com/api/v1/ownerships-changes-stats/docs">https://sos2425-19.onrender.com/api/v1/ownerships-changes-stats/docs</a> (developed by Julián Villaescusa Fernández)</p>
-    <p><a href="https://sos2425-19.onrender.com/api/v1/accident-rate-2023-stats/docs"> https://sos2425-19.onrender.com/api/v1/accident-rate-stats/docs</a> (developed by Mario Rodriguez Carrillo)
+    <p><a href="https://sos2425-19.onrender.com/api/v1/accident-rate-stats/docs"> https://sos2425-19.onrender.com/api/v1/accident-rate-stats/docs</a> (developed by Mario Rodriguez Carrillo)
     </p>
