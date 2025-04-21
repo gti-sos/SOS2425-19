cc521d2539b74eee24afe78c53de1e8dbda66f79
julvilfer1 <julvilfer1@alum.us.es>
2025-04-21 19:13:01 +0200

    arreglo links

--- src/front/src/routes/+page.svelte
+++ src/front/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <h1>SOS2425 GRUPO 19</h1>
     <p><a href="https://sos2425-19.onrender.com/about/">Project About</a></p>
     <p><a href="https://sos2425-19.onrender.com/api/v1/sanctions-and-points-stats/">API-DLC</a></p>
-    <p><a href="https://sos2425-19.onrender.com/api/v1/ownerships-changes-stats/">API-JVF</a></p>
+    <p><a href="https://sos2425-19.onrender.com/api/v1/ownerships-changes-stats/">API-JVF-v1</a></p>
+    <p><a href="https://sos2425-19.onrender.com/api/v2/ownerships-changes-stats/">API-JVF-v2</a></p>
     <p><a href="https://sos2425-19.onrender.com/api/v1/accident-rate-stats/">API-MRC</a></p>
     
     <p> Daniel Lebrero Carcela,<b>Fuente de datos:</b> sanctions-and-points-stats</p>
@@ -9,7 +10,6 @@
     <p> Julián Villaescusa Fernández,<b>Fuente de datos:</b> ownerships-changes-stats </p>
     
     <p><a href="https://sos2425-19.onrender.com/api/v1/sanctions-and-points-stats/docs">https://sos2425-19.onrender.com/api/v1/sanctions-and-points-stats/docs</a> (developed by Daniel Lebrero Carcela)</p>
-    <p><a href="https://sos2425-19.onrender.com/api/v1/ownerships-changes-stats/docs">https://sos2425-19.onrender.com/api/v1/ownerships-changes-stats/docs</a> (developed by Julián Villaescusa Fernández)</p>
     <p><a href="https://sos2425-19.onrender.com/api/v2/ownerships-changes-stats/docs">https://sos2425-19.onrender.com/api/v2/ownerships-changes-stats/docs</a> (developed by Julián Villaescusa Fernández)</p>
     <p><a href="https://sos2425-19.onrender.com/api/v1/accident-rate-stats/docs"> https://sos2425-19.onrender.com/api/v1/accident-rate-stats/docs</a> (developed by Mario Rodriguez Carrillo)
     </p>
