98ef5e499dd30d892e9376137b9b376f40749a7d
julvilfer1 <114571923+julvilfer1@users.noreply.github.com>
2025-04-21 12:54:16 +0200

    Update +page.svelte

--- src/front/src/routes/+page.svelte
+++ src/front/src/routes/+page.svelte
@@ -5,6 +5,8 @@
     <p><a href="https://sos2425-19.onrender.com/api/v1/accident-rate-stats/">API-MRC</a></p>
     
     <p> Daniel Lebrero Carcela,<b>Fuente de datos:</b> sanctions-and-points-stats</p>
+    <p> Mario Rodriguez Carrillo,<b>Fuente de datos:</b> accident-rate-stats</p>
+    <p> Julián Villaescusa Fernández,<b>Fuente de datos:</b> ownerships-changes-stats </p>
     
     <p><a href="https://sos2425-19.onrender.com/api/v1/sanctions-and-points-stats/docs">https://sos2425-19.onrender.com/api/v1/sanctions-and-points-stats/docs</a> (developed by Daniel Lebrero Carcela)</p>
     <p><a href="https://sos2425-19.onrender.com/api/v1/ownerships-changes-stats/docs">https://sos2425-19.onrender.com/api/v1/ownerships-changes-stats/docs</a> (developed by Julián Villaescusa Fernández)</p>
