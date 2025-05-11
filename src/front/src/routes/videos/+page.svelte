f2fa5c7d35f6be2d7121c4e5e43388a6b5bb4a75
julvilfer1 <julvilfer1@alum.us.es>
2025-05-11 23:41:15 +0200

    video

--- src/front/src/routes/videos/+page.svelte
+++ src/front/src/routes/videos/+page.svelte
@@ -1,2 +1,3 @@
 <p><a href="https://www.youtube.com/watch?v=9jKHBMnC6uE"> Video DLC</a></p>
 <p><a href="https://www.youtube.com/watch?v=qjLntJPxAko"> Video MRC</a></p>
+<p><a href="https://youtu.be/cde6vHeDFVY"> Video JVF</a></p>
