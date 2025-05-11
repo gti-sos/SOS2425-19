a801f1373b647f3626869c15b613d6ca69f0b651
DaniLebrero <danilebrecar07@gmail.com>
2025-05-11 21:44:11 +0200

    Cambios analytics

---
+++ src/front/src/routes/analytics/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    //@ts-nocheck
+    import {onMount} from "svelte";
+    import {dev} from "$app/environment"
+    import { Table,Button } from '@sveltestrap/sveltestrap';
+
+    let DEVEL_HOST = "http://localhost:16078";
+    let API = "/api/v1/sanctions-and-points-stats";
+    if(dev){
+        API = DEVEL_HOST + API
+    }
+    let sanctionsData = [];
+    let result ="";
+    let resultStatus ="";
+    async function getSanctions() {
+        resultStatus = result = "";
+        try {
+            const res = await fetch(API, {method:"GET"});        
+            const data = await res.json();
+            result = JSON.stringify(data,null,2);
+            console.log(result)
+            sanctionsData = data;
+        } catch (error) {
+            console.log(`ERROR: GET data from ${API}: ${error}`)
+        }
+    }
+
+    onMount(async () => {
+    await getSanctions();
+});
+</script>
+    
